44bfe19c4acbe68caff62d3e040041e1207257ea
Adhiraj16a12 <64204448+Adhiraj16a12@users.noreply.github.com>
2021-10-29 15:12:00 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,25 @@
+from flask import Flask , jsonify , request
+from Data import data
+
+
+app = Flask(__name__)
+@app.route("/")
+
+def index():
+    return jsonify({
+        "data" : data,
+        "message" : 'gg'
+    })
+
+@app.route("/planet")
+
+def planet():
+    name = request.args.get("name")
+    planet_data = next(item for item in data if item["name"]== name)
+    return jsonify({
+        "data" : planet_data , 
+        "message" : "sucessfull"
+    }), 200
+
+if __name__ == "__main__":
+    app.run() 
